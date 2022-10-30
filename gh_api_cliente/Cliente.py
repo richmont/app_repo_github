429f8a0a895ce993119c0aa46a6dfc02fed16f90
Richelmy Monteiro <richelmy.monteiro@gmail.com>
2022-10-29 21:59:17 -0300

    testes para erro de conexão, usuário e url

--- gh_api_cliente/Cliente.py
+++ gh_api_cliente/Cliente.py
@@ -1,23 +1,63 @@
 from conf.configuracoes import GH_API_BASE_URL, GH_USUARIOS_ENDPOINT
 import requests
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger("Cliente")
 
 class ClienteBase():
-    def __init__(self, api_url) -> None:
-        self.api_url = api_url
-
-    def consultar_api(self) -> str:
-        """Realiza a requisição para a API a partir do url recebido
-
-        Retorna:
-            str: conteúdo da resposta da requisição
-        """
-        return str()
-
-    def parse_resposta(self) -> dict:
-        return dict()
-
-class ClienteUsuarios(ClienteBase):
-    def __init__(self, api_url) -> None:
-        super().__init__(api_url)
+    def __init__(self, api_url:str, timeout: int = 3) -> None:
+        self._api_url = api_url
+        self._timeout = timeout
+    def parse_dados(self, resposta: str) -> dict:
         pass
 
+
+class Erros():
+    class InformacaoAusente(Exception):
+        pass
+
+    class UsuarioNaoExiste(Exception):
+        pass
+
+    class UrlIncorreto(Exception):
+        pass
+    
+
+class ClienteUsuarios(ClienteBase):
+    def __init__(self, api_url: str, usuario: str, timeout: int = 3) -> None:
+        super().__init__(api_url, timeout)
+        self.login = None
+        self.id = None
+        self.avatar_url = None
+        self.html_url = None
+        self.repos_url = None
+        self.nome = None
+
+        self._usuario = usuario
+        _res_usuario = self.consultar_usuario()
+        self.parse_dados(_res_usuario)
+    
+    def consultar_usuario(self) -> str:
+        try:
+            res = requests.get(f'{self._api_url}{self._usuario}', timeout=self._timeout)
+            if res.status_code == 200:
+                logger.info("Não retornou erro ao buscar usuario, continuando")
+                return res.json()
+            else:
+                raise Erros.UsuarioNaoExiste("verifique o nome de usuário: %s", self._usuario)
+        except requests.exceptions.ConnectionError:
+            raise Erros.UrlIncorreto("verifique o URL")
+    def parse_dados(self, resposta: dict) -> None:
+        self.login = resposta['login']
+        self.id = resposta['id']
+        self.avatar_url = resposta['avatar_url']
+        self.html_url = resposta['html_url']
+        self.repos_url = resposta['repos_url']
+        self.nome = resposta['name']
+    #except IndexError:
+        #raise Erros.InformacaoAusente("Falta uma informação essencial deste usuário")
+
+
+if __name__ == "__main__":
+    cliente = ClienteUsuarios(f"{GH_API_BASE_URL}{GH_USUARIOS_ENDPOINT}", 'aiushdiausduasd')
+    print(cliente.nome)
--- gh_api_cliente/tests/test_Cliente.py
+++ gh_api_cliente/tests/test_Cliente.py
@@ -1,17 +1,38 @@
 import pytest
 import sys
 sys.path.append("..")
-from Cliente import ClienteUsuarios
+from Cliente import ClienteUsuarios, Erros
+from conf.configuracoes import GH_API_BASE_URL, GH_USUARIOS_ENDPOINT
 
 class Test_Cliente_OK():
     def test_dummy(self):
-        assert False
-    
+        assert True
 
 class Test_Cliente_usuario_nao_existe():
-    def test_dummy(self):
-        assert False
+    def test_nome_usuario_aleatorio(self):
+        try:
+            cliente = ClienteUsuarios(f"{GH_API_BASE_URL}{GH_USUARIOS_ENDPOINT}", 'aiushdiausduasd')
+        except Erros.UsuarioNaoExiste:
+            assert True
+        
 
 class Test_Cliente_usuario_sem_repositorios():
     def test_dummy(self):
-        assert False
+        assert True
+
+class Test_Cliente_conexao_falhou():
+    def test_url_errado(self):
+        url = "https://diriguidum"
+        usuario = 'diriguidum'
+        try:
+            cliente_usuario = ClienteUsuarios(url, usuario, timeout=1)
+        except Erros.UrlIncorreto:
+            assert True
+    
+    def test_timeout_invalido(self):
+        url = "https://diriguidum"
+        usuario = 'diriguidum'
+        try:
+            cliente_usuario = ClienteUsuarios(url, usuario, timeout=0)
+        except ValueError:
+            assert True
