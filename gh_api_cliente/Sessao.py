3a547ec8ccf25130f1dc99114639810fe8d9de72
Richelmy Monteiro <richelmy.monteiro@gmail.com>
2022-10-31 23:45:06 -0300

    Sessão melhor descrita e gitignore inclui dotenv

--- gh_api_cliente/Sessao.py
+++ gh_api_cliente/Sessao.py
@@ -1,15 +1,17 @@
 from requests import Session
 from conf.configuracoes import PERSONAL_TOKEN, LOGIN
 
-def _sessao_getter():
-    return SESSAO
+"""
+Define uma sessão comum, com login predefinido, para realizar todas as requisições
+"""
 
 SESSAO = Session()
-Session = _sessao_getter()
-
+def _sessao_getter():
+    return SESSAO
 
 if LOGIN is not None or PERSONAL_TOKEN is not None:
     user = str(LOGIN)
     password = str(PERSONAL_TOKEN)
     SESSAO.auth = (user, password)
+Session = _sessao_getter()
 
