7310f0b9fb4d5f37066bfbb1b7c9f7b35bbf20ca
richelmy <richelmy@debian-BULLSEYE-live-builder-AMD64>
2022-11-03 17:31:07 -0300

    filtragem pelo nome do repositorio

--- proj_repos_github/app_repos_github/static/index/js/index.js
+++ proj_repos_github/app_repos_github/static/index/js/index.js
@@ -1,1 +1,29 @@
 console.log('hello world');
+//var lista_repositorios = document.getElementById('lista-repositorios')
+const obterListaRepositorios = (id_lista) => {
+    var listaRepositorios = document.getElementById(id_lista).children
+  
+    return listaRepositorios
+  }
+
+  listaRepositorios = obterListaRepositorios('lista-repositorios');
+  
+  for (let repo of listaRepositorios)
+  { 
+    console.log(repo.querySelector('.repo-nome').innerText)
+    }
+  
+  const barraBusca = document.getElementById('barraBusca');
+  barraBusca.addEventListener('input', (e)=> {
+      const stringBusca = e.target.value.toLowerCase();
+      console.log(stringBusca);
+      for (let repo of listaRepositorios){ 
+        if(repo.querySelector('.repo-nome').innerText.toLowerCase().includes(stringBusca)){
+          continue
+        } else{
+          repo.style.display = 'none'
+        }
+    }
+      
+  });
+  
--- proj_repos_github/manage.py
+++ proj_repos_github/manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+sys.path.append(os.environ['VIRTUAL_ENV'])
 
 def main():
     """Run administrative tasks."""
