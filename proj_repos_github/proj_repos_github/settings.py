eda54849860b3a94f4a1212e5ec748fe0adb8ade
Richelmy Monteiro <richelmy.monteiro@gmail.com>
2022-12-01 19:19:20 -0300

    alertas do bootstrap adicionados, botão fork me para parte inferior

--- proj_repos_github/proj_repos_github/settings.py
+++ proj_repos_github/proj_repos_github/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -70,6 +70,13 @@ TEMPLATES = [
     },
 ]
 
+MESSAGE_TAGS = {
+        messages.DEBUG: 'secondary',
+        messages.INFO: 'info',
+        messages.SUCCESS: 'success',
+        messages.WARNING: 'warning',
+        messages.ERROR: 'danger',
+ }
 
 
 WSGI_APPLICATION = 'proj_repos_github.wsgi.application'
